9e389e290b54a5903dd129c7ba4b88f923e2aef1
alperenkarate <alperensozen96@gmail.com>
2021-10-02 15:47:05 +0300

    created task-item component for tasklist

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Hello Motherfucker!';
+  title: string = 'Todo w/ Angular!';
 
   constructor() { }
 
