c8a158397d3c8a30cfeb262268380d34c2129cf0
alperenkarate <alperensozen96@gmail.com>
2021-10-02 18:21:03 +0300

    ngIf is used to conditional showing add-task component

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import {Task} from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text: string = "";
   day: string = "";
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    if(!this.text){
+      alert("please add a task!");
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Todo w/ Angular!';
+  showAddTask: boolean = false;
+  subscrtiption!: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) { 
+    this.subscrtiption = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log("ali")
+    this.uiService.toggleAddTask();
   }
 }
